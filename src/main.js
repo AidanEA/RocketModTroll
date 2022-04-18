12410689b9fd8d59f266645d96165d96e93178bd
Aidan Adams <aieadams@ucsc.edu>
2022-04-18 15:04:53 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 // reserve keyboard vars
+//Aidan Adams
+//Rocket Patrol Mod
+// 4/18/22
+//~7 hours?
+
+//POINT BREAKDOWN
+//Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (60)
+//Create and implement a new weapon (w/ new behavior and graphics) (20)
+//Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (20)
+
+//I recieved assistance from Jawad in getting my Laser weapon functionality working
 let keyF, keyR, keyLEFT, keyRIGHT;
 let keyC;
 let config = {
